3c7d0c0b044bbb047cc9039de7ab93cbde629a17
Franz Zemen <franzzemen@gmail.com>
2022-07-21 13:53:24 -0400

    inference stack parser

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,4 +1,5 @@
 export * from './util/external-index';
 export * from './rule-element-ref/external-index';
 export * from './scoped-factory/external-index';
+export * from './inference-stack-parser/external-index';
 
