4a96786f6deaba562777838ea4ebc8a082c6f852
Franz Zemen <franzzemen@gmail.com>
2022-07-29 07:39:57 -0400

    scope refactor

--- ts-src/scope/scope.ts
+++ ts-src/scope/scope.ts
@@ -8,15 +8,15 @@ export class Scope extends Map<string, any> {
     super();
   }
 
-  getScopedFactory<C>(scope: Map<string, any>, refName: string, factoryKey: string, searchParent = true, ec?: ExecutionContextI) : C {
-    const factory: ScopedFactory<C> = scope.get(factoryKey);
+  getScopedFactory<C>(refName: string, factoryKey: string, searchParent = true, ec?: ExecutionContextI) : C {
+    const factory: ScopedFactory<C> = this.get(factoryKey);
     const c = factory.getRegistered(refName, ec);
     if(c) {
       return c;
     } else if (searchParent) {
-      const parentScope = scope.get(Scope.ParentScope) as Map<string, any>;
+      const parentScope = this.get(Scope.ParentScope) as Scope;
       if(parentScope) {
-        return this.getScopedFactory<C>(parentScope, refName, factoryKey, searchParent, ec);
+        return parentScope.getScopedFactory<C>(refName, factoryKey, searchParent, ec);
       }
     }
     return undefined;
