aa418953555d5fca1d1d370245eb96135872f1bd
Franz Zemen <franzzemen@gmail.com>
2022-08-21 10:25:21 -0400

    npmu

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,23 @@
-
+const join = require('path').join;
 const gulpBase = require('@franzzemen/gulp-base').init(require('./package.json'), 100, true);
 require('@franzzemen/gulp-base').setMainBranch('main');
 
+const npmu = require('@franzzemen/npmu').npmu;
+
+exports.npmu = (cb) => npmu([
+  {
+    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/gulp-base',
+  }, {
+    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/safe-config',
+  }, {
+    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/npmu',
+  }, {
+    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/app-utility',
+  }])
+  .then(() => {
+    console.log('cb...');
+    cb();
+  });
 
 exports.buildTest = gulpBase.buildTest;
 exports.test = gulpBase.test;
