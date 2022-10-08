eb207f00fa0668af5577314bed61236c40747e63
Franz Zemen <franzzemen@gmail.com>
2022-10-08 14:08:39 -0400

    _mergeOptions

--- ts-src/scope/options.ts
+++ ts-src/scope/options.ts
@@ -3,6 +3,13 @@ export interface Options {
   throwOnAsync?: boolean;
 }
 
+export function _mergeOptions(target: Options, source: Options, modifyTarget = true): Options {
+  let _target: Options = modifyTarget ? {} : target;
+  _target.name = source.name ? source.name : target.name;
+  _target.throwOnAsync = source.throwOnAsync ? source.throwOnAsync : target.throwOnAsync;
+  return target;
+}
+
 export const defaultOptions: Options = {
   throwOnAsync: false
 }
