489d17e228441a4a3c6366f65fe1e1b0e43696a7
Franz Zemen <franzzemen@gmail.com>
2022-10-10 10:45:36 -0400

    cliImplementation

--- ts-src/cli-common.ts
+++ ts-src/cli-common.ts
@@ -10,31 +10,13 @@ import {RuleElementFactory} from './rule-element-ref/rule-element-factory.js';
 
 export type CliFunction = (args: string[], ec?: ExecutionContextI) => void;
 
-export function execute(key: string, cliFunction: CliFunction, defaultEC?: ExecutionContextI) {
-  const log = new LoggerAdapter(defaultEC, 're-common', 'cli-common', 'execute');
-  log.debug(process.argv, 'argv');
-  if(process.argv[2] !== key) {
-    log.error(`Parameters must start with keyword ${key}`);
-    process.exit(3);
-  }
-  let args = process.argv.slice(3);
-  log.debug(args, 'args');
-  let ec: ExecutionContextI;
-  try {
-    const configContainer = loadJSONResource({moduleName: '../../../cli.json', moduleResolution: ModuleResolution.json}, defaultEC);
-    if(isPromise(configContainer)) {
-      log.error('Unsupported dynamic JSON load or schema validation');
-      process.exit(2);
-    } else if(isAppConfigSync(configContainer) && configContainer?.log) {
-      ec = {config: configContainer};
-    } else {
-      ec = defaultEC;
+const defaultEC: ExecutionContextI = {
+  config: {
+    log: {
+      level: LoggerAdapter.lvl_info
     }
-  } catch (err) {
-    ec = defaultEC;
   }
-  cliFunction(args, ec);
-}
+};
 
 export class CliImplementation {
   constructor(public commandLineKey: string, public cliFunction: CliFunction) {
@@ -53,3 +35,39 @@ export class CliFactory extends RuleElementFactory<CliImplementation> {
 }
 
 export const defaultCliFactory = new CliFactory();
+
+
+export function execute() {
+  let ec: ExecutionContextI;
+  try {
+    const configContainer = loadJSONResource({moduleName: '../../../cli.json', moduleResolution: ModuleResolution.json}, defaultEC);
+    if(isPromise(configContainer)) {
+      const log = new LoggerAdapter(defaultEC, 're-common', 'cli-common', 'execute');
+      log.error('Unsupported dynamic JSON load or schema validation');
+      process.exit(2);
+    } else if(isAppConfigSync(configContainer) && configContainer?.log) {
+      ec = {config: configContainer};
+    } else {
+      ec = defaultEC;
+    }
+  } catch (err) {
+    ec = defaultEC;
+  }
+  const log = new LoggerAdapter(ec, 're-common', 'cli-common', 'execute');
+
+  log.debug(process.argv, 'argv');
+  if(process.argv.length < 3) {
+    log.error(`Parameters must start with keyword`);
+    process.exit(3);
+  }
+  const keyword = process.argv[2];
+  log.debug(`keyword ${keyword} found`);
+  const cliImpl: CliImplementation = defaultCliFactory.getRegistered(keyword, ec);
+  if(!cliImpl) {
+    log.error(`keyword not in CliFactory`);
+    process.exit(4);
+  }
+  let args = process.argv.slice(3);
+  log.debug(args, 'args');
+  cliImpl.cliFunction(args, ec);
+}
